b42a7fc0cf94a4822e2d78eec761fd058e06360a
Yebin <ybyb99@naver.com>
2024-02-12 21:36:39 +0900

    Feat: 애니메이션 구조 짜기

--- frontend/chipchippoker/src/components/Play/PlayBatting.vue
+++ frontend/chipchippoker/src/components/Play/PlayBatting.vue
@@ -70,6 +70,17 @@
         </div>
       </div>
       
+      <video
+        style="z-index: 2000;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 1920px;
+        height: 1080px;"
+          src="/src/assets/icons/startvideo.mp4" alt="비디오 없다" controls autoplay muted>
+          이 비디오를 열람할 수 없습니다
+      </video>
+
       <!-- 아래쪽 -->
       <div class="row col-12 justify-content-between" v-if="gameStore.memberInfos && gameStore.memberInfos.length > 2">
 
@@ -148,6 +159,7 @@
 import { useGameStore } from '@/stores/game';
 import { watch,computed, ref } from 'vue';
 const gameStore = useGameStore()
+
 const getCoinUrl = function (num) {
   return new URL(`/src/assets/coins/coin${num}.png`, import.meta.url).href;
 };
@@ -169,6 +181,20 @@ const updateData = function(){
 const nextRoundState = computed(() => gameStore.nextRoundState); // 현재 라운드 상태 (ex. 진행중)
 const nextCurrentRound = computed(() => gameStore.nextCurrentRound) // 현재 라운드
 
+const gameStart = ref(false)
+// 게임 시작 애니메이션
+const startGameAnimation = function () {
+    // 1. 플레이페이지 진입하면 텍스트 애니메이션 (3초 정도)
+    console.log('게임 비디오 시작')
+    gameStart.value = true
+    setTimeout(()=>{
+      gameStart.value = false
+      console.log(gameStart.value);
+    },3000)
+    // 2. 애니메이션 종료 되면 startRoundAnimation() 호출
+
+}
+
 // 라운드 상태 감지
 watch(() => nextRoundState.value, (newValue, oldValue) => {
   console.log('라운드 변경');
@@ -176,6 +202,7 @@ watch(() => nextRoundState.value, (newValue, oldValue) => {
     if (gameStore.nextCurrentRound === 1) {
       console.log('게임시작');
       // 게임 시작
+      startGameAnimation()
       updateData()
       
     } else {
@@ -198,6 +225,64 @@ watch(() => nextYourTurn.value, (newValue, oldValue) => {
   console.log('턴 변경');
   updateData()
 })
+
+
+// 라운드 시작 콜백함수
+const startRoundAnimation = function () {
+    // 1. 화면 가운데 카드 생성 애니메이션 (createCard)
+    
+    
+    // 2. 각 플레이어에게 카드 분배 애니메이션 (moveCard 카드 이동)
+    
+    
+    // 3. 분배 완료 후 카드 사라짐 (FadeoutCard)
+}
+
+// 라운드 종료 콜백함수
+const endRoundAnimation = function () {
+  // 1. 
+
+  // 2. 
+
+  // 3.
+}
+
+// 카드 생성 (가운데)
+const createCard = function () {
+
+}
+
+// 카드 분배
+const moveCard = function () {
+
+}
+
+// 카드 제거 (희미하게 사라짐)
+const removeCard = function () {
+
+}
+
+// 카드 뒤집기
+const flipCard = function () {
+
+}
+
+// 카드 모으기
+const joinCard = function () {
+
+}
+
+// 코인 배팅
+const bettingCoin = function () {
+
+}
+
+// 코인 이동 (승자에게)
+const joinCoin = function () {
+
+}
+
+
 
 </script>
 
